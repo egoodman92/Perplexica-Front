6464de15308d2b072f3269bba0868ac35eb572c8
Emmett Goodman <egoodman92@gmail.com>
2025-07-20 21:30:25 -0700

    facelift - UI improvements

--- src/components/EmptyChat.tsx
+++ src/components/EmptyChat.tsx
@@ -2,8 +2,6 @@ import { Settings } from 'lucide-react';
 import EmptyChatMessageInput from './EmptyChatMessageInput';
 import { File } from './ChatWindow';
 import Link from 'next/link';
-import WeatherWidget from './WeatherWidget';
-import NewsArticleWidget from './NewsArticleWidget';
 
 const EmptyChat = ({
   sendMessage,
@@ -33,10 +31,10 @@ const EmptyChat = ({
           <Settings className="cursor-pointer lg:hidden" />
         </Link>
       </div>
-      <div className="flex flex-col items-center justify-center min-h-screen max-w-screen-sm mx-auto p-2 space-y-4">
+      <div className="flex flex-col items-center justify-center min-h-screen max-w-screen-sm mx-auto p-2">
         <div className="flex flex-col items-center justify-center w-full space-y-8">
           <h2 className="text-black/70 dark:text-white/70 text-3xl font-medium -mt-8">
-            Research begins here.
+            Find Parts.
           </h2>
           <EmptyChatMessageInput
             sendMessage={sendMessage}
@@ -49,14 +47,6 @@ const EmptyChat = ({
             files={files}
             setFiles={setFiles}
           />
-        </div>
-        <div className="flex flex-col w-full gap-4 mt-2 sm:flex-row sm:justify-center">
-          <div className="flex-1 w-full">
-            <WeatherWidget />
-          </div>
-          <div className="flex-1 w-full">
-            <NewsArticleWidget />
-          </div>
         </div>
       </div>
     </div>
