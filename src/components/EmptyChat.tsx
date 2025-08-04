7e8184ecc8bc4309cb2891ac140762c7815d8b0d
Emmett Goodman <egoodman92@gmail.com>
2025-08-03 21:23:10 -0500

    add bobbles back

--- src/components/EmptyChat.tsx
+++ src/components/EmptyChat.tsx
@@ -2,6 +2,8 @@ import { Settings } from 'lucide-react';
 import EmptyChatMessageInput from './EmptyChatMessageInput';
 import { File } from './ChatWindow';
 import Link from 'next/link';
+import WeatherWidget from './WeatherWidget';
+import NewsArticleWidget from './NewsArticleWidget';
 
 const EmptyChat = ({
   sendMessage,
@@ -47,6 +49,14 @@ const EmptyChat = ({
             files={files}
             setFiles={setFiles}
           />
+        </div>
+        <div className="flex flex-col w-full gap-4 mt-2 sm:flex-row sm:justify-center">
+          <div className="flex-1 w-full">
+            <WeatherWidget />
+          </div>
+          <div className="flex-1 w-full">
+            <NewsArticleWidget />
+          </div>
         </div>
       </div>
     </div>
